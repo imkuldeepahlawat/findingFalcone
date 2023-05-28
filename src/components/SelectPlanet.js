55e09b1b1e26ac4472be6421c339eac39d9a4a74
imkuldeepahlawat <imkuldeepahlawat@hotmail.com>
2023-05-28 06:36:01 +0530

    refactor

--- src/components/SelectPlanet.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-const SelectPlanet = () => {
-    
-}
-
-export default SelectPlanet;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -33,7 +21,6 @@ root.render(
         <App />
       </SnackbarProvider>
     </BrowserRouter>
->>>>>>> master
   </React.StrictMode>
 );
 
